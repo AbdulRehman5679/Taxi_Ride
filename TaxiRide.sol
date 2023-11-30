6eeec60c259c81752b9486e3fe72cc9ad1714269
Mani8588 <moonrana173@yahoo.com>
2023-11-30 10:36:28 +0000

    Final commit(Rearranging)

--- TaxiRide.sol
+++ TaxiRide.sol
@@ -1,31 +1,36 @@
-//SPDX-License-identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.7.5;
 
-contract TaxiRide {
-    
-    enum RideStatus {Requested, Completed} 
-Struct Ride {
+contract Practice {
+   
+
+  struct Ride {
         address passenger;
         string destination;
         uint256 timestamp;
         RideStatus status;
-}
- mapping(uint256=> Ride) public rides;
- uint256 public rideCount;
-    event RideRequested(uint256 rideId, address passenger, string destination, uint256 timestamp);
-    event RideCompleted(uint256 rideId, uint256 timestamp);
+    }
 
-     function requestRide(string memory _destination) external {
+   enum RideStatus { Requested, Completed }
+
+   
+      mapping(uint256 => Ride) public rides;
+    uint256 public rideCount;
+
+ event RideRequested(uint256 rideId, address passenger, string destination, uint256 timestamp);
+function requestRide(string memory _destination) external {
         rideCount++;
         Ride storage newRide = rides[rideCount];
         newRide.passenger = msg.sender;
         newRide.destination = _destination;
         newRide.timestamp = block.timestamp;
+        newRide.status = RideStatus.Requested;
 
         emit RideRequested(rideCount, msg.sender, _destination, block.timestamp);
-}
-uint public balanceReceived;
+    }
+
+ uint public balanceReceived;
 
     function receivedMoney() public payable {
         balanceReceived += msg.value;
@@ -39,17 +44,17 @@ uint public balanceReceived;
             _to.transfer(getBalance());
         
     }
+    
     event RideCompleted(uint256 rideId, uint256 timestamp);
-    
-    function completeRide(uint256_rideId) external{
-    require(_rideId > 0 && _rideId <= ridecount, "Invalid ride ID");    
-    Ride storage ride = rides [_rideId];
-    require (ride.passenger == msg.sender, "unauthorised");
-    require (ride.status == Ridestatus.Requested, "ride is not requested");
-    
-    ride.status=RideStatus.Completed;
 
-    emit Ridecompleted(_rideId, block.timestamp);
-}
-    
+    function completeRide(uint256 _rideId) external {
+        require(_rideId > 0 && _rideId <= rideCount, "Invalid ride ID");
+        Ride storage ride = rides[_rideId];
+        require(ride.passenger == msg.sender, "Unauthorized");
+        require(ride.status == RideStatus.Requested, "Ride is not requested");
+
+        ride.status = RideStatus.Completed;
+
+        emit RideCompleted(_rideId, block.timestamp);
+    }
 }
